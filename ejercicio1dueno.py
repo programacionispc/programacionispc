bfb9e1a4b1801913e634b09eeba9dfdc1d0bb80f
programacionispc <111255282+programacionispc@users.noreply.github.com>
2022-08-14 22:01:30 -0300

    Add files via upload

---
+++ ejercicio1dueno.py
@@ -0,0 +1,26 @@
+import math 
+
+
+
+numero = int (input("ingrese un numero"))
+dueno_lista  = [" 28957346","Juan","Perez","4789689","Belgrano101"]
+
+if numero < 26000000:
+    
+    print("el numero es menor a 26000000")
+   
+if numero> 26000000:
+    print(" el numero de telefono es 4789689")
+
+
+
+
+
+
+
+
+
+
+       
+  
+         
